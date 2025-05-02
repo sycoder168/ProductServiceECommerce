1dce305d5e101bd0a0c8d7b3ed5683c343538da4
Siddhartha Yenni <sycoder1123@gmail.com>
2025-05-02 09:29:21 +0530

    Product Service initialized.

---
+++ src/main/java/com/scaler/productservicecapstone/ProductServiceCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicecapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/productservicecapstone/ProductServiceCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.productservicecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
