50ec074b8ef6d29b2853662a53ac60743a60d0ce
Siddhartha Yenni <sycoder1123@gmail.com>
2025-05-02 10:13:21 +0530

    Product and Category models created

---
+++ src/main/java/com/scaler/productservicecapstone/models/Product.java
@@ -0,0 +1,14 @@
+package com.scaler.productservicecapstone.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Category category;
+}
