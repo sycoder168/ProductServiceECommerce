38269ed41181c082b1b594c669022787b2206403
Siddhartha Yenni <sycoder1123@gmail.com>
2025-05-11 22:41:42 +0530

    Created Base model and extended it to Category and Product classes. Modified application.properties to connect to mysql database

--- src/main/java/com/scaler/productservicecapstone/models/Category.java
+++ src/main/java/com/scaler/productservicecapstone/models/Category.java
@@ -1,11 +1,16 @@
 package com.scaler.productservicecapstone.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class Category {
-    private long id;
-    private String name;
+@Entity
+public class Category extends Base {
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
--- src/main/java/com/scaler/productservicecapstone/models/Product.java
+++ src/main/java/com/scaler/productservicecapstone/models/Product.java
@@ -1,15 +1,18 @@
 package com.scaler.productservicecapstone.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    private long id;
-    private String name;
+@Entity
+public class Product extends Base {
     private double price;
     private String description;
     private String imageUrl;
+    @ManyToOne
     private Category category;
 }
